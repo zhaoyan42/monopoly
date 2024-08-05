908510f9e4bdaf42d2d4c1b83068979ecefb28e4
赵晏龙 <kitten63928@live.com>
2024-08-05 23:25:44 +0800

    状态提升

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,18 @@ import styled from 'styled-components';
 import { Board } from './Board.tsx';
 import { useConfirm } from './hooks/useConfirm.tsx';
 import { useAlert } from './hooks/useAlert.tsx';
+import { useImmer } from 'use-immer';
+import { Player } from './model/Player.ts';
+import { countryList } from './model/Country.ts';
+import {
+  createConfirmContent,
+  createOwnAlertContent,
+  createPurchaseAlertContent,
+  createRentAlertContent,
+} from './utils/contentFactory.tsx';
+import { useOwnedCountriesStore } from './store/ownedCountriesStore.ts';
+import { useRef, useState } from 'react';
+import { CountryChain } from './model/CountryChain.ts';
 
 const GameWrapper = styled('div')`
   height: 100%;
@@ -12,13 +24,80 @@ const GameWrapper = styled('div')`
 `;
 
 function App() {
+  const countryChain = useRef(new CountryChain(countryList));
+
   const { ConfirmComponent } = useConfirm();
   const { AlertComponent } = useAlert();
 
+  const [players, setPlayers] = useImmer([
+    new Player('#ff0000', countryList[0]),
+    new Player('#00ff00', countryList[0]),
+    new Player('#0000ff', countryList[0]),
+  ]);
+
+  const [currentPlayerIndex, setCurrentPlayerIndex] = useState(0);
+
+  const { confirm } = useConfirm();
+  const { alert } = useAlert();
+  const { purchaseCountry, isCountryPurchased, getOwner } =
+    useOwnedCountriesStore();
+
+  function rollDice() {
+    return Math.floor(Math.random() * 6) + 1;
+  }
+
+  async function roll() {
+    const currentPlayer = players[currentPlayerIndex];
+    const steps = rollDice();
+    console.log(`%c${steps}`, `color: ${currentPlayer.color};`);
+
+    const targetCountry = countryChain.current.getTargetCountry(
+      currentPlayer.country,
+      steps,
+    );
+    setPlayers((draft) => {
+      draft[currentPlayerIndex].country = targetCountry;
+    });
+
+    if (!isCountryPurchased(targetCountry)) {
+      if (
+        await confirm({
+          content: createConfirmContent(targetCountry),
+          confirmText: '购买',
+          cancelText: '不要',
+        })
+      ) {
+        purchaseCountry(currentPlayer, targetCountry);
+        alert({
+          content: createPurchaseAlertContent(currentPlayer, targetCountry),
+        });
+      } else {
+        console.log(
+          `%c玩家 ${currentPlayer.color} 选择不买 ${targetCountry.name}`,
+          `color: ${currentPlayer.color};`,
+        );
+      }
+    } else {
+      const owner = getOwner(targetCountry);
+      if (!owner) throw new Error('Country should have an owner');
+      if (owner === currentPlayer) {
+        alert({
+          content: createOwnAlertContent(currentPlayer, targetCountry),
+        });
+      } else {
+        alert({
+          content: createRentAlertContent(currentPlayer, targetCountry, owner),
+        });
+      }
+    }
+
+    setCurrentPlayerIndex((index) => (index + 1) % players.length);
+  }
+
   return (
     <>
-      <GameWrapper>
-        <Board></Board>
+      <GameWrapper onClick={roll}>
+        <Board players={players} currentPlayerIndex={currentPlayerIndex} />
       </GameWrapper>
       <ConfirmComponent />
       <AlertComponent />
--- src/Board.tsx
+++ src/Board.tsx
@@ -1,21 +1,9 @@
 import { WorldMap } from './model/Map.ts';
 import { Country, countryList } from './model/Country.ts';
 import { CountrySquare, EmptySquare } from './Square.tsx';
-import { useRef, useState } from 'react';
+import { useRef } from 'react';
 import { Player } from './model/Player.ts';
-import { CountryChain } from './model/CountryChain.ts';
 import styled from 'styled-components';
-import { useImmer } from 'use-immer';
-
-import { useConfirm } from './hooks/useConfirm.tsx';
-import { useOwnedCountriesStore } from './store/ownedCountriesStore.ts';
-import { useAlert } from './hooks/useAlert.tsx';
-import {
-  createPurchaseAlertContent,
-  createOwnAlertContent,
-  createRentAlertContent,
-  createConfirmContent,
-} from './utils/contentFactory.tsx';
 
 const BoardWrapper = styled('div')<{ $currentPlayerColor: string }>`
   flex-grow: 1;
@@ -39,104 +27,35 @@ const BoardWrapper = styled('div')<{ $currentPlayerColor: string }>`
   }
 `;
 
-export function Board() {
+type BoardProps = { players: Player[]; currentPlayerIndex: number };
+
+export function Board({ players, currentPlayerIndex }: BoardProps) {
   const worldMap = useRef(new WorldMap(countryList));
-  const countryChain = useRef(new CountryChain(countryList));
-
-  const [players, setPlayers] = useImmer([
-    new Player('#ff0000', countryList[0]),
-    new Player('#00ff00', countryList[0]),
-    new Player('#0000ff', countryList[0]),
-  ]);
-
-  const [currentPlayerIndex, setCurrentPlayerIndex] = useState(0);
-
-  const { confirm } = useConfirm();
-  const { alert } = useAlert();
-  const { purchaseCountry, isCountryPurchased, getOwner } =
-    useOwnedCountriesStore();
 
   function getPlayers(country: Country) {
     return players.filter((player) => player.country === country);
   }
 
-  function rollDice() {
-    return Math.floor(Math.random() * 6) + 1;
-  }
-
-  async function roll() {
-    const currentPlayer = players[currentPlayerIndex];
-    const steps = rollDice();
-    console.log(`%c${steps}`, `color: ${currentPlayer.color};`);
-
-    const targetCountry = countryChain.current.getTargetCountry(
-      currentPlayer.country,
-      steps,
-    );
-    setPlayers((draft) => {
-      draft[currentPlayerIndex].country = targetCountry;
-    });
-
-    if (!isCountryPurchased(targetCountry)) {
-      if (
-        await confirm({
-          content: createConfirmContent(targetCountry),
-          confirmText: '购买',
-          cancelText: '不要',
-        })
-      ) {
-        purchaseCountry(currentPlayer, targetCountry);
-        alert({
-          content: createPurchaseAlertContent(currentPlayer, targetCountry),
-        });
-      } else {
-        console.log(
-          `%c玩家 ${currentPlayer.color} 选择不买 ${targetCountry.name}`,
-          `color: ${currentPlayer.color};`,
-        );
-      }
-    } else {
-      const owner = getOwner(targetCountry);
-      if (!owner) throw new Error('Country should have an owner');
-      if (owner === currentPlayer) {
-        alert({
-          content: createOwnAlertContent(currentPlayer, targetCountry),
-        });
-      } else {
-        alert({
-          content: createRentAlertContent(currentPlayer, targetCountry, owner),
-        });
-      }
-    }
-
-    setCurrentPlayerIndex((index) => (index + 1) % players.length);
-  }
-
   return (
-    <>
-      <BoardWrapper
-        onClick={roll}
-        $currentPlayerColor={players[currentPlayerIndex].color}
-      >
-        {Array(9)
-          .fill(0)
-          .flatMap((_, y) =>
-            Array(16)
-              .fill(0)
-              .map((_, x) => {
-                const country = worldMap.current.getCountry(x, y);
-                return country ? (
-                  <CountrySquare
-                    key={`${y}-${x}`}
-                    country={country}
-                    players={getPlayers(country)}
-                  />
-                ) : (
-                  <EmptySquare key={`${y}-${x}`}></EmptySquare>
-                );
-              }),
-          )}
-      </BoardWrapper>
-    </>
+    <BoardWrapper $currentPlayerColor={players[currentPlayerIndex].color}>
+      {Array(9)
+        .fill(0)
+        .flatMap((_, y) =>
+          Array(16)
+            .fill(0)
+            .map((_, x) => {
+              const country = worldMap.current.getCountry(x, y);
+              return country ? (
+                <CountrySquare
+                  key={`${y}-${x}`}
+                  country={country}
+                  players={getPlayers(country)}
+                />
+              ) : (
+                <EmptySquare key={`${y}-${x}`}></EmptySquare>
+              );
+            }),
+        )}
+    </BoardWrapper>
   );
 }
